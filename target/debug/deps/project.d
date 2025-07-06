b1752f3431be561623416f936cbf9f2b485cac65
Geomi <geosteam123@outlook.com>
2025-07-06 22:54:07 +0500

    Added main.rs yea just a file but time is 11PM, I go to sleep

---
+++ target/debug/deps/project.d
@@ -0,0 +1,5 @@
+C:\Users\Geomi\RustroverProjects\TermOS\target\debug\deps\project.d: src\main.rs
+
+C:\Users\Geomi\RustroverProjects\TermOS\target\debug\deps\project.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/project.d
@@ -0,0 +1,1 @@
+C:\Users\Geomi\RustroverProjects\TermOS\target\debug\project.exe: C:\Users\Geomi\RustroverProjects\TermOS\src\main.rs
